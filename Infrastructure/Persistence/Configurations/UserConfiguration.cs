12e1b1d76af0d141e63bc1fcb94bc7e4eecd11a8
jonas <jonasroder@gmail.com>
2025-04-29 16:18:44 -0300

    configure

--- Infrastructure/Persistence/Configurations/UserConfiguration.cs
+++ Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,11 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Persistence.Configurations
 {
-    internal class UserConfiguration
+    public class UserConfiguration : IEntityTypeConfiguration<User>
     {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
