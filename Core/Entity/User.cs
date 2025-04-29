035efe0926e65fd8e42f35b1bd0a479760db99b5
jonas <jonasroder@gmail.com>
2025-04-29 12:19:02 -0300

    adress

--- Core/Entity/User.cs
+++ Core/Entity/User.cs
@@ -7,6 +7,8 @@ namespace Core.Entity
         public required string Name { get; set; }
         public required DateTime DateOfBirth { get; set; }
         public required Email Email { get; set; }
+        public required string Phone { get; set; }
+        public required Address Address { get; set; }
         public required Password Password { get; set; }
     }
 }
---
+++ Core/ValueObjects/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ValueObjects
+{
+    public sealed class Address
+    {
+        public string Street { get; }
+        public string City { get; }
+        public string State { get; }
+        public string ZipCode { get; }
+        public string Country { get; }
+    }
+}
